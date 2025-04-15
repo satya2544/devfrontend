46097915b2ee25d56d540839e836266df0dcc5de
satya2544 <koppolu.satya@gmail.com>
2025-04-15 16:38:10 +0530

    hello world

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1>Hello world</h1>
+    </>
+  )
+}
+
+export default App
